b75bbaccf5d56ff52c726765b7b7851eb03eb212
rajsubhajit554-cloud <rajsubhajit554-cloud@users.noreply.github.com>
2026-08-19 11:19:27 +0530

    Independence Day Special theme & poster update

--- script.js
+++ script.js
@@ -250,7 +250,7 @@ if (document.readyState === 'loading') {
     initPreloader();
 }
 
-// Rath Yatra Restaurant Feast Click-to-Activate Animation
+// Independence Day Special Feast Click-to-Activate Animation
 const templeBell = document.getElementById('temple-bell');
 const rathAltar = document.getElementById('rath-feast-altar');
 if (templeBell && rathAltar) {
